b63d181d1bff88de6d3360e5e7a3861a64b97e12
abusayed2050 <abu.sayed2050@gmail.com>
2026-03-10 01:54:41 +0600

    navbar components

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Link from './Link';
+const NavBar = () => {
+    
+    const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/"
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about"
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services"
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog"
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact"
+  }
+];
+
+    return (
+        <nav>
+           <ul className='flex'> 
+                {navLinks.map(route=><Link key={route.id} route={route}></Link>)}
+           </ul>
+            <ul className='flex'>
+                <li className='mr-10'><a href="/">Home</a></li>
+                <li className='mr-10'><a href="/about">About</a></li>
+                <li className='mr-10'><a href="/blog">Blog</a></li>
+            </ul>
+             <ul className='flex'>
+                {navLinks.map(route=><li className='mr-10'>
+                    <a href={route.path}>{route.name}</a>
+                    </li>)}
+            </ul>
+        </nav>
+            
+        
+    );
+};
+
+export default NavBar;
